5c3c428e3a6e87b61af7d2d9e86304f7f85b90bd
Julius B. Kirkegaard <juliusbierk@gmail.com>
2025-09-11 12:46:12 +0200

    Headless cross-platform testing is a pain.

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import pytest
-
-@pytest.fixture(scope="session", autouse=True)
-def _force_offscreen_qt_in_ci():
-    # Use offscreen when no display is available (e.g., CI/Linux)
-    if os.environ.get("CI") == "true" or not os.environ.get("DISPLAY"):
-        os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
